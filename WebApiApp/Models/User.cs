9d03447894ef3d312525bffdcaa00f31111801f4
Daniil Oborin <gigasale.test@gmail.com>
2022-05-08 20:32:24 +0300

    Use model validation

--- WebApiApp/Models/User.cs
+++ WebApiApp/Models/User.cs
@@ -1,9 +1,14 @@
-﻿namespace WebApiApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiApp.Models
 {
     public sealed class User
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Укажите имя пользователя")]
         public string Name { get; set; }
+        [Range(1, 100, ErrorMessage = "Возраст должен быть в промежутке от 1 до 100")]
+        [Required(ErrorMessage = "Укажите возраст пользователя")]
         public int Age { get; set; }
     }
 }

d0d4e2db32a4b741f13867a5e7c04791025298bd
Daniil Oborin <gigasale.test@gmail.com>
2022-05-08 20:12:24 +0300

    Added a new project with UsersController

---
+++ WebApiApp/Startup.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApiApp.Models;
+
+namespace WebApiApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string con = "Server=(localdb)\\mssqllocaldb;Database=usersdbstore;Trusted_Connection=True;";
+            // устанавливаем контекст данных
+            services.AddDbContext<UsersContext>(options => options.UseSqlServer(con));
+
+            services.AddControllers(); // используем контроллеры без представлений
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers(); // подключаем маршрутизацию на контроллеры
+            });
+        }
+    }
+}
